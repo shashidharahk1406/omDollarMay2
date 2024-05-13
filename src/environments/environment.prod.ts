ec21d6dc00e15b09f54710aa70598661b67ad720
shashidharahk1406 <shashidhara@ekfrazo.in>
2024-05-13 10:26:22 +0530

    taken pull from master branch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // live_url:'https://omdollar.thestorywallcafe.com',
-  live_url:' https://omdollardev.thestorywallcafe.com',
+  live_url:'https://omdollar.thestorywallcafe.com',
+  // live_url:' https://omdollardev.thestorywallcafe.com',
   // live_url:'https://omdollars.com'
 
 };
